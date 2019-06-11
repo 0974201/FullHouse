21494e067d454c9ff4703faf5cd77f15b6ca090a
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2019-06-12 00:22:56 +0200

    Update and rename src/fullhousedb21/SimpleDataSourceV2.java to src/fullhousedb21/5/SimpleDataSourceV2.java

--- src/fullhousedb21/5/SimpleDataSourceV2.java
+++ src/fullhousedb21/5/SimpleDataSourceV2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fullhousedb21;
 
 import java.sql.Connection;
@@ -12,7 +7,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Suman
+ * @author adeliae
  */
 public class SimpleDataSourceV2 {
 
@@ -44,7 +39,7 @@ public class SimpleDataSourceV2 {
         }
 		
         dbserver = "";
-        database = "13010751";
+        database = "";
         username = "13010751";
         password = "";
     }
