205455376b49c573d00f3dd1886e07d5976dffe7
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2019-06-12 00:20:34 +0200

    Update and rename src/fullhousedb21/MasterclassAanmaken.java to src/fullhousedb21/5/MasterclassAanmaken.java

--- src/fullhousedb21/5/MasterclassAanmaken.java
+++ src/fullhousedb21/5/MasterclassAanmaken.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fullhousedb21;
 
 import java.sql.SQLException;
@@ -11,7 +6,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Suman
+ * @author adeliae
  */
 public class MasterclassAanmaken extends javax.swing.JFrame {
     
