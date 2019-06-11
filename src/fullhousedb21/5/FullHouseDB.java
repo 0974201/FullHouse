135836319c5e2156610870e6e4d91bd529494380
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2019-06-12 00:17:08 +0200

    q

--- src/fullhousedb21/5/FullHouseDB.java
+++ src/fullhousedb21/5/FullHouseDB.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fullhousedb21;
 
 import javax.swing.JFrame;
 
 /**
  *
- * @author Suman
+ * @author adeliae
  */
 public class FullHouseDB {
 
