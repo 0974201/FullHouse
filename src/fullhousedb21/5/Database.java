29a97e55e531daa53edd93c2dca3545c152e04df
Sushi <31491170+cyborgrooster@users.noreply.github.com>
2019-06-12 00:16:28 +0200

    Update and rename src/fullhousedb21/Database.java to src/fullhousedb21/5/Database.java

--- src/fullhousedb21/5/Database.java
+++ src/fullhousedb21/5/Database.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fullhousedb21;
 
 import java.sql.Connection;
@@ -21,7 +16,7 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author Suman
+ * @author adeliae
  */
 public class Database {
     
